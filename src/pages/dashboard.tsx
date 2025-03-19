a2360d72b903165efced51d1936e74ebeb1ffd36
Nicholas Myers <nicholasmyers.swe@gmail.com>
2025-03-18 21:54:11 -0700

    Infra: Add typescript. Start Navbar

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Navbar from "../layouts/Navbar/Navbar";
+
+const Dashboard: React.FC = () => {
+    return (
+        <Navbar />
+    )
+};
+
+export default Dashboard;
